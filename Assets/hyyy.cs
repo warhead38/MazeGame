72fefdbd38bcf3d73e22eea7957f7eb6eadb7c82
warhead38 <42575004+warhead38@users.noreply.github.com>
2019-05-25 12:05:26 +0530

    maze game

---
+++ Assets/hyyy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hyyy : MonoBehaviour {
+	public Transform playerTransform;
+	Vector3 offset;
+		
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - playerTransform.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = playerTransform.transform.position + offset;	
+	}
+}
---
+++ Assets/yoyo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yoyo : MonoBehaviour {
+	int a;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal= Input.GetAxis("Horizontal");
+		float moveVertical= Input.GetAxis("Vertical");                                     
+		Vector3 force = new Vector3 (moveHorizontal,0.0f,moveVertical);
+		rb.AddForce (force*500.0f*Time.deltaTime);
+		if(Input.GetKeyDown(KeyCode.Space)){
+                   Vector3 jump = new Vector3 (0.0f,5.0f,0.0f);
+                   rb.AddForce (jump, ForceMode.Impulse);
+          }
+	}
+}
+
